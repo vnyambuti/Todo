4ab0547f54d964008e0725f5912ff40315be21bc
vnyambuti <vnyambuti@gmail.com>
2025-04-15 13:22:45 +0300

    project structure and domain entities

---
+++ Todo.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Todo.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<TodoItem>? TodoItems { get; set; }
+    }
+}
---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+namespace Todo.Domain.Entities
+{
+   public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Note { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool IsDone { get; set; }
+
+        public int? categoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+
+    }
+}
